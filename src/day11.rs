6665f95e0a34e11d2e62eafb567e933bafe72391
Finlay Wojtan <finlaywojtan@microsoft.com>
2023-12-11 10:58:42 +0000

    Solve day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,71 +1,47 @@
 use std::collections::HashMap;
 
-use petgraph::{algo::dijkstra, stable_graph::NodeIndex, Graph, Undirected};
+use petgraph::{algo::dijkstra, dot::Dot, stable_graph::NodeIndex, Graph, Undirected};
 
 use crate::Solution;
 
 #[derive(Clone, Debug)]
 pub struct Day11;
 
+pub struct Day11Data {
+    graph: Graph<char, u64, Undirected>,
+    galaxies: Vec<NodeIndex>,
+    node_indices: Vec<Vec<NodeIndex>>,
+    rows_to_expand: Vec<usize>,
+    cols_to_expand: Vec<usize>,
+}
+
 impl Solution for Day11 {
-    type ParsedInput = (Graph<char, (), Undirected>, Vec<NodeIndex>);
+    type ParsedInput = Day11Data;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         // Change the return type of this function by editing the ParsedInput type above.
         // You can skip this and pass the raw string to each part.
         // Alternatively, you can parse the input here, either working on the same mutable struct
         // in parts one and two or passing a tuple with the data required for each part.
-        let row_major_universe = input_lines
+        let universe = input_lines
             .lines()
             .map(|line| line.chars().collect::<Vec<char>>())
             .collect::<Vec<Vec<char>>>();
 
-        let mut partially_expanded_row_major_universe =
-            Vec::with_capacity(row_major_universe.len() * 2);
-        for row in row_major_universe {
-            if !row.contains(&'#') {
-                partially_expanded_row_major_universe.push(row.clone());
-            }
-            partially_expanded_row_major_universe.push(row);
-        }
-
-        let mut partially_expanded_column_major_universe =
-            vec![
-                Vec::with_capacity(partially_expanded_row_major_universe.len());
-                partially_expanded_row_major_universe[0].len() * 2
-            ];
-
-        for row in partially_expanded_row_major_universe {
-            for (idx, item) in row.iter().enumerate() {
-                partially_expanded_column_major_universe[idx].push(*item)
+        let mut col_maj_universe = vec![Vec::with_capacity(universe.len()); universe.len()];
+        for (row_idx, row) in universe.iter().enumerate() {
+            for (col_idx, item) in row.iter().enumerate() {
+                col_maj_universe[col_idx].push(*item)
             }
         }
 
-        let mut expanded_universe =
-            Vec::with_capacity(partially_expanded_column_major_universe.len() * 2);
-
-        for col in partially_expanded_column_major_universe {
-            if !col.contains(&'#') {
-                expanded_universe.push(col.clone());
-            }
-            expanded_universe.push(col);
-        }
-
-        let expanded_universe: Vec<_> = expanded_universe
-            .into_iter()
-            .filter(|col| !col.is_empty())
-            .collect();
-
-        let mut graph = Graph::<char, (), Undirected>::new_undirected();
+        let mut graph = Graph::<char, u64, Undirected>::new_undirected();
         let mut galaxies = Vec::new();
 
-        let mut node_indices =
-            vec![Vec::with_capacity(expanded_universe[0].len()); expanded_universe.len()];
+        let mut node_indices = vec![Vec::with_capacity(universe[0].len()); universe.len()];
 
-        println!("{:?}", expanded_universe);
-
-        for (idx, col) in expanded_universe.iter().enumerate() {
-            for item in col {
+        for (idx, row) in universe.iter().enumerate() {
+            for item in row {
                 let node = graph.add_node(*item);
                 if *item == '#' {
                     galaxies.push(node);
@@ -73,44 +49,81 @@ impl Solution for Day11 {
                 node_indices[idx].push(node);
             }
         }
-        // println!("{:?}", node_indices);
+        let rows_to_expand = universe
+            .iter()
+            .enumerate()
+            .filter(|(_row_idx, row)| !row.contains(&'#'))
+            .map(|(row_idx, _row)| row_idx)
+            .collect::<Vec<_>>();
+
+        let cols_to_expand = col_maj_universe
+            .iter()
+            .enumerate()
+            .filter(|(_col_idx, col)| !col.contains(&'#'))
+            .map(|(col_idx, _col)| col_idx)
+            .collect::<Vec<_>>();
 
         for (col_idx, col) in node_indices.iter().enumerate() {
             for (row_idx, item) in col.iter().enumerate() {
                 if let Some(next_col) = node_indices.get(col_idx + 1) {
-                    graph.update_edge(*item, next_col[row_idx], ());
+                    graph.update_edge(*item, next_col[row_idx], 1);
                 }
                 if let Some(next_item) = col.get(row_idx + 1) {
-                    graph.update_edge(*item, *next_item, ());
+                    graph.update_edge(*item, *next_item, 1);
                 }
             }
         }
 
-        (graph, galaxies)
+        Day11Data {
+            graph,
+            galaxies,
+            node_indices,
+            rows_to_expand,
+            cols_to_expand,
+        }
     }
 
     fn part_one(parsed_input: &mut Self::ParsedInput) -> String {
-        let mut pairings = HashMap::new();
-        println!("Number of galaxies: {}", parsed_input.1.len());
+        solve_for_expansion(parsed_input, 2)
+    }
 
-        for galaxy in parsed_input.1.iter() {
-            let distances = dijkstra(&parsed_input.0, *galaxy, None, |_| 1);
-            for (target_node, distance) in distances.into_iter() {
-                if *parsed_input.0.node_weight(target_node).unwrap() == '#' {
-                    pairings.insert(
-                        (*galaxy.min(&target_node), *galaxy.max(&target_node)),
-                        distance,
-                    );
-                }
+    fn part_two(parsed_input: &mut Self::ParsedInput) -> String {
+        solve_for_expansion(parsed_input, 1000000)
+    }
+}
+
+fn solve_for_expansion(parsed_input: &mut Day11Data, expansion_amount: u64) -> String {
+    for row_idx in &parsed_input.rows_to_expand {
+        for (col_idx, node_idx_a) in parsed_input.node_indices[*row_idx].iter().enumerate() {
+            parsed_input.graph.update_edge(
+                *node_idx_a,
+                parsed_input.node_indices[row_idx + 1][col_idx],
+                expansion_amount,
+            );
+        }
+    }
+
+    for col_idx in &parsed_input.cols_to_expand {
+        for row in parsed_input.node_indices.iter() {
+            parsed_input
+                .graph
+                .update_edge(row[*col_idx], row[col_idx + 1], expansion_amount);
+        }
+    }
+
+    let mut pairings = HashMap::new();
+    for galaxy in parsed_input.galaxies.iter() {
+        let distances = dijkstra(&parsed_input.graph, *galaxy, None, |e| *e.weight() as u64);
+        for (target_node, distance) in distances.into_iter() {
+            if *parsed_input.graph.node_weight(target_node).unwrap() == '#' {
+                pairings.insert(
+                    (*galaxy.min(&target_node), *galaxy.max(&target_node)),
+                    distance,
+                );
             }
         }
-        pairings.values().sum::<i32>().to_string()
     }
-
-    fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part two
-        0.to_string()
-    }
+    pairings.values().sum::<u64>().to_string()
 }
 
 #[cfg(test)]
@@ -137,8 +150,47 @@ mod tests {
     }
 
     #[test]
-    fn check_day11_part2_case1() {
-        assert_eq!(Day11::solve_part_two(""), "0".to_string())
+    fn check_day11_case2() {
+        assert_eq!(
+            solve_for_expansion(
+                &mut Day11::parse_input(
+                    "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."
+                ),
+                10
+            ),
+            "1030"
+        )
+    }
+
+    #[test]
+    fn check_day11_case3() {
+        assert_eq!(
+            solve_for_expansion(
+                &mut Day11::parse_input(
+                    "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."
+                ),
+                100
+            ),
+            "8410"
+        )
     }
 
     #[test]
